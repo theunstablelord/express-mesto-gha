de79a9df27c97400de8991d3b960f6af18c085a5
Artem Zakharov <kennyohmygod@yandex.ru>
2024-01-14 22:43:13 +0300

    ПР№13: создание схем и моделей, а также роутов и контроллеров

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  req.user = {
+    _id: '65a41790d672e67b24fddca1',
+  };
+
+  next();
+});
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+  useNewUrlParser: true,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
+});
+
+app.use(routes);
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
---
+++ controllers/cards.js
@@ -0,0 +1,78 @@
+const Card = require('../models/card');
+const { BAD_REQUEST, NOT_FOUND, SERVER_ERROR } = require('../utils/errors');
+
+module.exports.getCards = (req, res) => {
+  Card.find({})
+    .then((cards) => res.status(200).send(cards))
+    .catch(() => res.status(SERVER_ERROR).send({ message: 'Произошла ошибка на сервере'}));
+};
+
+module.exports.createCard = (req, res) => {
+  const { name, link } = req.body;
+  const owner = req.user._id;
+  
+  return Card.create({ name, link, owner })
+    .then((card) => res.status(200).send(card))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
+      } else {
+        res.status(SERVER_ERROR).send({ message: 'Произошла ошибка на сервере' });
+      }
+    });
+};
+
+module.exports.deleteCard = (req, res) => {
+  const { cardId } = req.params;
+
+  return Card.findByIdAndRemove(cardId)
+    .orFail(() => new Error('NotFound'))
+    .then((card) => res.status(200).send(card))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
+      } else if (err.message === 'NotFound') {
+        res.status(NOT_FOUND).send({ message: 'Карточка по указанному _id не найдена' });
+      } else {
+        res.status(SERVER_ERROR).send({ message: 'Произошла ошибка на сервере' });
+      }
+    });
+};
+
+module.exports.likeCard = (req, res) => {
+  Cards.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
+  )
+    .orFail(() => new Error('NotFound'))
+    .then((card) => res.status(200).send(card))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
+      } else if (err.message === 'NotFound') {
+        res.status(NOT_FOUND).send({ message: 'Передан несуществующий _id карточки' });
+      } else {
+        res.status(SERVER_ERROR).send({ message: 'Произошла ошибка на сервере' });
+      }
+    });
+};
+
+module.exports.dislikeCard = (req, res) => {
+  Cards.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true },
+  )
+    .orFail(() => new Error('NotFound'))
+    .then((card) => res.status(200).send(card))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
+      } else if (err.message === 'NotFound') {
+        res.status(NOT_FOUND).send({ message: 'Передан несуществующий _id карточки' });
+      } else {
+        res.status(SERVER_ERROR).send({ message: 'На сервере произошла ошибка' });
+      }
+    });
+};
